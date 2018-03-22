863dde642632e74af145da9725181b786cb2c5d4
zelenko <30665185+zelenko@users.noreply.github.com>
2018-03-21 20:49:40 -0400

    copy array, get random quotes

--- copy_array.js
+++ copy_array.js
@@ -1,7 +1,16 @@
 // copy array
 
 var arr = [1,2];
-//var arr2 = arr;
-var arr2 = Object.assign([], arr);
+
+// one way:
+// var arr2 = Object.assign([], arr);
+
+
 arr.push(3);
+
 console.log(arr)
+
+
+// or this here is another way to copy array
+arr2 = arr.slice();
+
